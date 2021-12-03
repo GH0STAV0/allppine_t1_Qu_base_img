42742dae8cf01665cf3b2fe5a67068a8cfe5f141
GH0STAV0 <you@example.com>
2021-12-03 08:39:14 +0800

    add new project

--- src/package_2.sh
+++ src/package_2.sh
@@ -10,7 +10,7 @@ echo "Install Package - on  based ubuntu 20.04 LTS"
 echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"
 #apt-get install -y dbus-x11 xauth xinit x11-xserver-utils xdg-utils--no-cache=true
 wget http://launchpadlibrarian.net/494460182/xfwm4_4.14.5-1_amd64.deb
-dpkg -i xfwm4_4.14.5-1_amd64.deb
+#dpkg -i xfwm4_4.14.5-1_amd64.deb
 apt update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen openvpn autocutsel xfwm4=4.14.5-1 python3-pip python3.9-tk python3.9-dev tor
 

0c7407c7632b3c507892b66d94c0ebb705e0b781
GH0STAV0 <you@example.com>
2021-12-03 08:45:38 +0800

    add new project

--- src/package_2.sh
+++ src/package_2.sh
@@ -11,8 +11,9 @@ echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"
 #apt-get install -y dbus-x11 xauth xinit x11-xserver-utils xdg-utils--no-cache=true
 wget http://launchpadlibrarian.net/494460182/xfwm4_4.14.5-1_amd64.deb
 #dpkg -i xfwm4_4.14.5-1_amd64.deb
+#xfwm4=4.14.5-1
 apt update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen openvpn autocutsel xfwm4=4.14.5-1 python3-pip python3.9-tk python3.9-dev tor
+DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen openvpn autocutsel xfwm4 python3-pip python3.9-tk python3.9-dev tor
 
 mkdir /root/EXTRAT/firefox-53.0.2/
 mkdir /root/EXTRAT/firefox-53.0b9/

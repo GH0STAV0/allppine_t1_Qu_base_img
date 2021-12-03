19ba1f036d29916862d1c504e7994bc059d6d934
GH0STAV0 <you@example.com>
2021-12-03 20:45:00 +0800

    add new project

--- src/package.sh
+++ src/package.sh
@@ -23,28 +23,28 @@ tar xzf Python-3.9.4.tgz
 cd Python-3.9.4 
 ./configure --enable-optimizations 
 make altinstall 
-python3 --version
+python3.9.4 --version
 cd ..
 
  # python3 python3-dev
 # update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
 # update-alternatives --config python3
 # echo "Installing ttf-wqy-zenhei"
-# apt-get install -y ttf-wqy-zenhei software-properties-common \
+apt-get install -y ttf-wqy-zenhei software-properties-common \
 
-# apt-get upgrade -y && apt-get install -y \
-#     xfwm4 xfce4-session xfce4-panel \
-#     xfce4-terminal xfce4-appfinder \
-#     xfce4-goodies xfce4-pulseaudio-plugin \
-#     xfce4-statusnotifier-plugin xfce4-whiskermenu-plugin \
-#     thunar tumbler xarchiver \
-#     mugshot thunar-archive-plugin
-# echo "Installing  xfce "
-# apt-get install -y  task-xfce-desktop dbus-x11 xauth xinit x11-xserver-utils xdg-utils \
-#         xfce4 tightvncserver novnc websockify \
-#         python3-numpy build-essential net-tools curl git \
-#         software-properties-common xfce4-terminal xvfb gedit screen 
-#         #uuid-runtime
+apt-get upgrade -y && apt-get install -y \
+    xfwm4 xfce4-session xfce4-panel \
+    xfce4-terminal xfce4-appfinder \
+    xfce4-goodies xfce4-pulseaudio-plugin \
+    xfce4-statusnotifier-plugin xfce4-whiskermenu-plugin \
+    thunar tumbler xarchiver \
+    mugshot thunar-archive-plugin
+echo "Installing  xfce "
+apt-get install -y  task-xfce-desktop dbus-x11 xauth xinit x11-xserver-utils xdg-utils \
+        xfce4 tightvncserver novnc websockify \
+        python3-numpy build-essential net-tools curl git \
+        software-properties-common xfce4-terminal xvfb gedit screen 
+        #uuid-runtime
 
 
 # echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"

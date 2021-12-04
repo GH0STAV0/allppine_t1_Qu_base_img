4924cca23f25f37554f0d4faa097af4bcf76ebe7
GH0STAV0 <you@example.com>
2021-12-04 09:53:41 +0800

    add new project

--- src/package.sh
+++ src/package.sh
@@ -3,62 +3,6 @@ export USER="root"
 
 ### every exit != 0 fails the script
 set -e
-#python3 --version
-echo "Install Package - on  based ubuntu 20.04 LTS"
-DEBIAN_FRONTEND=noninteractive apt-get remove python3
-apt autoremove -y
-apt update --fix-missing
-
-DEBIAN_FRONTEND=noninteractive apt-get -f install  -y \
-        jq \
-        nano \
-        psmisc \
-        sudo \
-        tini \
-        software-properties-common python3 python3-dev \
-        wget openssh-server locate nano 
-
-apt-get clean autoclean
-apt-get autoremove --yes
-#rm -rf /var/lib/{apt,cache,log}/
-# wget https://www.python.org/ftp/python/3.9.4/Python-3.9.4.tgz 
-# tar xzf Python-3.9.4.tgz 
-# cd Python-3.9.4 
-# ./configure --enable-optimizations 
-# make altinstall 
-clear
-python3 --version
-echo "Installing ttf-wqy-zenhei"
-# cd ..
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen openvpn autocutsel xfwm4 python3-pip python3.9-tk python3.9-dev tor
-
- # python3 python3-dev
-# update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9.4 2
-# update-alternatives --config python3
-apt autoremove -y
-apt update --fix-missing
-echo "Installing ttf-wqy-zenhei"
-apt-get install -y ttf-wqy-zenhei software-properties-common \
-
-apt-get upgrade -y && apt-get install -y \
-    xfwm4 xfce4-session xfce4-panel \
-    xfce4-terminal xfce4-appfinder \
-    xfce4-goodies xfce4-pulseaudio-plugin \
-    xfce4-statusnotifier-plugin xfce4-whiskermenu-plugin \
-    thunar tumbler xarchiver \
-    mugshot thunar-archive-plugin
-echo "Installing  xfce "
-apt-get install -y  task-xfce-desktop dbus-x11 xauth xinit x11-xserver-utils xdg-utils \
-        xfce4 tightvncserver novnc websockify \
-        python3-numpy build-essential net-tools curl git \
-        software-properties-common xfce4-terminal xvfb gedit screen 
-        #uuid-runtime
-clear
-echo "clean "
-
-apt-get clean autoclean
-apt-get autoremove --yes
 
 echo "Installing  firefox "
 
@@ -93,6 +37,68 @@ wget https://ftp.mozilla.org/pub/firefox/releases/53.0b9/linux-x86_64/en-US/fire
 clear
 df
 echo "Install bash color"
+
+
+
+
+#python3 --version
+echo "Install Package - on  based ubuntu 20.04 LTS"
+DEBIAN_FRONTEND=noninteractive apt-get remove python3
+apt autoremove -y
+apt update --fix-missing
+
+DEBIAN_FRONTEND=noninteractive apt-get -f install  -y \
+        jq \
+        nano \
+        psmisc \
+        sudo \
+        tini \
+        software-properties-common python3 python3-dev \
+        supervisor pwgen openvpn autocutsel xfwm4 python3-pip python3.9-tk python3.9-dev tor \
+        wget openssh-server locate nano 
+
+apt-get clean autoclean
+apt-get autoremove --yes
+#rm -rf /var/lib/{apt,cache,log}/
+# wget https://www.python.org/ftp/python/3.9.4/Python-3.9.4.tgz 
+# tar xzf Python-3.9.4.tgz 
+# cd Python-3.9.4 
+# ./configure --enable-optimizations 
+# make altinstall 
+clear
+python3 --version
+echo "Installing ttf-wqy-zenhei"
+# cd ..
+apt-get update
+# DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen openvpn autocutsel xfwm4 python3-pip python3.9-tk python3.9-dev tor
+
+ # python3 python3-dev
+# update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9.4 2
+# update-alternatives --config python3
+apt autoremove -y
+apt update --fix-missing
+echo "Installing ttf-wqy-zenhei"
+apt-get install -y ttf-wqy-zenhei software-properties-common \
+
+apt-get upgrade -y && apt-get install -y \
+    xfwm4 xfce4-session xfce4-panel \
+    xfce4-terminal xfce4-appfinder \
+    xfce4-goodies xfce4-pulseaudio-plugin \
+    xfce4-statusnotifier-plugin xfce4-whiskermenu-plugin \
+    thunar tumbler xarchiver \
+    mugshot thunar-archive-plugin
+echo "Installing  xfce "
+apt-get install -y  task-xfce-desktop dbus-x11 xauth xinit x11-xserver-utils xdg-utils \
+        xfce4 tightvncserver novnc websockify \
+        python3-numpy build-essential net-tools curl git \
+        software-properties-common xfce4-terminal xvfb gedit screen 
+        #uuid-runtime
+clear
+echo "clean "
+
+apt-get clean autoclean
+apt-get autoremove --yes
+
 
 #apt 
 #rm -rf /var/lib/{apt,dpkg,cache,log}/

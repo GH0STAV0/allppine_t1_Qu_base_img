ce5f09bbfab858373b7325e7416561999c0d43aa
GH0STAV0 <you@example.com>
2021-12-04 10:23:49 +0800

    add new project

--- src/package_2.sh
+++ src/package_2.sh
@@ -22,7 +22,7 @@ pip3 install pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4   Confi
 
 # DEBIAN_FRONTEND=noninteractive apt-get install -y  \
 #         python3-numpy \
-&& mkdir -p "${NO_VNC_HOME}"/utils/websockify \
+mkdir -p "${NO_VNC_HOME}"/utils/websockify \
 && wget -qO- https://github.com/novnc/noVNC/archive/v1.2.0.tar.gz | tar xz --strip 1 -C "${NO_VNC_HOME}" \
 && wget -qO- https://github.com/novnc/websockify/archive/v0.9.0.tar.gz | tar xz --strip 1 -C "${NO_VNC_HOME}"/utils/websockify \
 && chmod +x -v "${NO_VNC_HOME}"/utils/*.sh \

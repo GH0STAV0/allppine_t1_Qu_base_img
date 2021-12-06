ae21f7d6623fc8e26813afcb66c90254dd3fee8a
GH0STAV0 <you@example.com>
2021-12-06 10:30:35 +0200

    add new project

--- src/package_3.sh
+++ src/package_3.sh
@@ -19,7 +19,7 @@ echo "Installing  firefox "
 whoami
 
 mkdir -p /root/EXTRAT/
-
+mkdir /root/EXTRAT/firefox-49.0b9/
 mkdir /root/EXTRAT/firefox-53.0.2/
 mkdir /root/EXTRAT/firefox-53.0b9/
 mkdir /root/EXTRAT/firefox-57.0.1/
@@ -29,7 +29,11 @@ mkdir /root/EXTRAT/firefox-60.0.1esr/
 mkdir /root/EXTRAT/firefox-61.0.1/
 mkdir /root/EXTRAT/firefox-63.0.1/
 #https://ftp.mozilla.org/pub/firefox/releases/53.0.2/linux-x86_64-EME-free/en-GB/firefox-53.0.2.tar.bz2
+
 echo "Install Package -firefox-53.0b9.tar.bz2"
+#https://ftp.mozilla.org/pub/firefox/releases/49.0b9/linux-x86_64/en-US/firefox-49.0b9.tar.bz2
+wget https://ftp.mozilla.org/pub/firefox/releases/49.0b9/linux-x86_64/en-US/firefox-49.0b9.tar.bz2 && tar -xf firefox-49.0b9.tar.bz2 -C /root/EXTRAT/firefox-49.0b9/
+
 wget https://ftp.mozilla.org/pub/firefox/releases/53.0.2/linux-x86_64-EME-free/en-GB/firefox-53.0.2.tar.bz2 && tar -xf firefox-53.0.2.tar.bz2 -C /root/EXTRAT/firefox-53.0.2/
 wget https://ftp.mozilla.org/pub/firefox/releases/53.0b9/linux-x86_64/en-US/firefox-53.0b9.tar.bz2 && tar -xf firefox-53.0b9.tar.bz2 -C /root/EXTRAT/firefox-53.0b9/
 wget https://ftp.mozilla.org/pub/firefox/releases/57.0.1/linux-x86_64/en-GB/firefox-57.0.1.tar.bz2 && tar -xf firefox-57.0.1.tar.bz2 -C /root/EXTRAT/firefox-57.0.1/
